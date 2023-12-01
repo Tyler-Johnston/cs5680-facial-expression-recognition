4127cbb176bb4c9aa7340f0fe06018efb56b714c
Tyler Johnston <johnstontyler@protonmail.com>
2023-12-01 16:11:16 -0700

    modified program after using the correct dataset

--- ckdata.py
+++ ckdata.py
@@ -1,100 +1,86 @@
-import pandas as pd
-import numpy as np
+import os
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from skimage.feature import local_binary_pattern
 
-'''
-CK Data CSV Format: emotion number, pixels, usage
-
-- Emotions/Expressions are defined as determined index below:
-
-    0 : Anger
-    1 : Disgust
-    2 : Fear
-    3 : Happiness
-    4 : Sadness
-    5 : Surprise
-    6 : Neutral
-    7 : Contempt
-
-- Pixels contains 2304 pixels (48x48) for each row
-
-- Usage is determined as Training(80%) / PublicTest(10%) / PrivateTest(10%)
-'''
-
-# LBP feature extraction function
-# Each array in the LBP feature set represents the histogram of LBP patterns for a given face region,
-def extractLbpFeatures(image):
-    radius = 1  # Radius of the circle
-    numberOfPoints = 8 * radius  # Number of points to consider around the circle
-    lbpImage = local_binary_pattern(image, numberOfPoints, radius, method='uniform')
-    numberOfBins = int(lbpImage.max() + 1)
-    lbpHistogram, _ = np.histogram(lbpImage.ravel(), bins=numberOfBins, range=(0, numberOfBins), density=True)
-
-    # # Display the LBP image
-    # plt.imshow(lbpImage, cmap='gray')
-    # plt.title("Local Binary Pattern")
-    # plt.axis('off')
-    # plt.show()
-
-    return lbpHistogram
-
-# ORB feature extraction function
-def extractOrbFeatures(image):
-    # orb = cv2.ORB_create(nfeatures=10, edgeThreshold=10)
-    orb = cv2.ORB_create()
-    keypoints, descriptors = orb.detectAndCompute(image, None)
-
-    # Draw keypoints on the image
-    keypoint_image = cv2.drawKeypoints(image, keypoints, None, color=(0, 255, 0), flags=0)
-
-    # # Display the image using matplotlib
-    # plt.imshow(cv2.cvtColor(keypoint_image, cv2.COLOR_BGR2RGB))
-    # plt.title("Image with ORB Keypoints")
-    # plt.axis('off')
-    # plt.show()
-    return descriptors
-
 # Load the Haar Cascade for face detection
-# The paper did not use Haar Cascade, but it noted that this was an option. 
-# I had issues using the dlib library that the actually used, and this alternative is working as expected
-cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cascadePath = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
-# Load the CSV file
-ckCSV = pd.read_csv("datasets/ckextended.csv")
+# Function to extract LBP features
+def extractLbpFeatures(faceImage):
+    radius = 1  # Radius of the LBP operation
+    nPoints = 8 * radius  # Number of points to consider
+    lbp = local_binary_pattern(faceImage, nPoints, radius, method="uniform")
+    nBins = int(lbp.max() + 1)
+    hist, _ = np.histogram(lbp.ravel(), bins=nBins, range=(0, nBins), density=True)
+    
+    # Display the LBP image
+    plt.imshow(lbp, cmap='gray')
+    plt.title("LBP Image")
+    plt.axis('off')
+    plt.show()
+    
+    return hist
 
-# Initialize lists to store LBP and ORB features
-lbpFeaturesList = []
-orbFeaturesList = []
+# Function to extract ORB features
+def extractOrbFeatures(faceImage):
+    orb = cv2.ORB_create()
+    keypoints, descriptors = orb.detectAndCompute(faceImage, None)
+    
+    # Draw keypoints on the image
+    if keypoints:
+        keypointImage = cv2.drawKeypoints(faceImage, keypoints, None, color=(0, 255, 0), flags=0)
+        plt.imshow(keypointImage)
+        plt.title("ORB Keypoints")
+        plt.axis('off')
+        plt.show()
+    
+    return keypoints, descriptors
 
-# for i in range(len(ckCSV)):
-for i in range(5):
-    pixels = ckCSV.iloc[i]['pixels']
-    pixels = list(map(int, pixels.split()))
-    image = np.array(pixels).reshape(48, 48).astype(np.uint8)
+# Main function to process images in each emotion folder
+def processEmotionImages(baseFolder):
+    emotions = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
+    features = {}
 
-    faces = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for emotion in emotions:
+        emotionFolder = os.path.join(baseFolder, emotion)
+        lbpFeaturesList = []
+        orbFeaturesList = []
 
-    for (x, y, w, h) in faces:
-        # Extract the face region from the image
-        extractedFace = image[y:y+h, x:x+w]
+        for filename in os.listdir(emotionFolder):
+            if filename.endswith('.jpg') or filename.endswith('.png'):
+                imagePath = os.path.join(emotionFolder, filename)
+                image = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+                
+                # Detect faces in the image
+                faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+                if len(faces) == 0:
+                    continue  # Skip images where no face is detected
+                x, y, w, h = faces[0]  # Use the first detected face
+                faceImage = image[y:y+h, x:x+w]  # Extract the face region
 
-        # # Display the extracted face region
-        # plt.imshow(extractedFace, cmap='gray')
-        # plt.title(f"Extracted Face from Image {i+1}")
-        # plt.axis('off')
-        # plt.show()
+                # Extract features
+                lbpFeatures = extractLbpFeatures(faceImage)
+                _, orbFeatures = extractOrbFeatures(faceImage)
 
-        # Extract LBP and ORB features
-        lbpFeatures = extractLbpFeatures(extractedFace)
-        orbFeatures = extractOrbFeatures(extractedFace)
+                lbpFeaturesList.append(lbpFeatures)
+                if orbFeatures is not None:
+                    orbFeaturesList.append(orbFeatures)
 
-        # Store the features in the lists
-        if lbpFeatures is not None:
-            lbpFeaturesList.append(lbpFeatures)
-        if orbFeatures is not None:
-            orbFeaturesList.append(orbFeatures)
+        features[emotion] = {
+            'LBP': lbpFeaturesList,
+            'ORB': orbFeaturesList
+        }
 
-print("LBP: ", lbpFeaturesList)
-print("ORB: ", orbFeaturesList)
+    return features
+
+# Specify the base folder where the emotion folders are located
+baseFolder = 'datasets/CK+'  # Update this with the path to your emotion folders
+allFeatures = processEmotionImages(baseFolder)
+
+# Now you can access the LBP and ORB features using allFeatures dictionary
+# For example, to print LBP features for 'anger' emotion:
+print("LBP Features for Anger:", allFeatures['anger']['LBP'])
+print("ORB Features for Anger:", allFeatures['anger']['ORB'])
