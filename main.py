c6703a1c71a50f1b2f7fd87adb7a430890de6fa8
Tyler Johnston <johnstontyler@protonmail.com>
2023-12-03 19:52:19 -0700

    added argument to specify the other models

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import cv2
 import numpy as np
 import pickle
 import argparse
-from utils import extractLbpFeatures, extractOrbFeatures, featureFusion  # Assuming these are in utils.py
+import matplotlib.pyplot as plt
+from utils import extractLbpFeatures, extractOrbFeatures
 
 def loadModel(modelPath):
     with open(modelPath, 'rb') as file:
         model = pickle.load(file)
     return model
 
-def preprocessImage(imagePath, faceCascade):
+def preprocessImage(imagePath, faceCascade, useOrb=False, useCombined=False):
     image = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 
     # Detect faces in the image
@@ -23,16 +24,25 @@ def preprocessImage(imagePath, faceCascade):
 
     # Extract features
     lbpFeatures = extractLbpFeatures(faceImage)
-    orbFeatures = extractOrbFeatures(faceImage)
+    orbFeatures = extractOrbFeatures(faceImage) if useOrb or useCombined else None
 
-    # If using a combined model, concatenate the features
-    combinedFeatures = np.concatenate([lbpFeatures, orbFeatures])
+    # Determine which features to use
+    if useCombined:
+        combinedFeatures = np.concatenate([lbpFeatures, orbFeatures])
+        return combinedFeatures
+    elif useOrb:
+        return orbFeatures
+    else:
+        return lbpFeatures
 
-    return combinedFeatures
-
-def main(imagePath):
-    # Load the trained model
-    modelPath = 'svmCombinedModel.pkl'
+def main(imagePath, modelType):
+    # Select model based on input
+    modelPaths = {
+        'lbp': 'svmLbpModel.pkl',
+        'orb': 'svmOrbModel.pkl',
+        'combined': 'svmCombinedModel.pkl'
+    }
+    modelPath = modelPaths[modelType]
     model = loadModel(modelPath)
 
     # Load the Haar Cascade for face detection
@@ -40,7 +50,7 @@ def main(imagePath):
     faceCascade = cv2.CascadeClassifier(cascadePath)
 
     # Preprocess the image
-    features = preprocessImage(imagePath, faceCascade)
+    features = preprocessImage(imagePath, faceCascade, useOrb=(modelType=='orb'), useCombined=(modelType=='combined'))
 
     # Reshape features for single sample prediction
     features = features.reshape(1, -1)
@@ -48,11 +58,17 @@ def main(imagePath):
     # Make a prediction
     prediction = model.predict(features)
 
-    # Output the result
-    print("Predicted Emotion:", prediction[0])
+    # Read the image and plot it
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.title(f"Predicted Emotion: {prediction[0]}")
+    plt.axis('off')
+    plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Facial Emotion Recognition')
     parser.add_argument('imagePath', type=str, help='Path to the image file')
+    parser.add_argument('--model', type=str, choices=['lbp', 'orb', 'combined'], default='combined', help='Model type (lbp, orb, or combined)')
     args = parser.parse_args()
-    main(args.imagePath)
+    main(args.imagePath, args.model)
